7ed0cbc6b3f3a6768cecdb6877f519490e24b01b
Taufiq R <taufiq.it2@gmail.com>
2019-10-16 06:36:04 +0700

    first commit

---
+++ src/pbo2/pkg10118080/latihan31/mahasiswa/oop/Mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118080.latihan31.mahasiswa.oop;
+
+/**
+ *
+ * @author Trizky
+ */
+public class Mahasiswa {
+    public String Nim,Nama;
+    
+    public void perkenalkanDiri(){
+        System.out.println("Hello Everyone");
+        System.out.println("My NIM is "+Nim);
+        System.out.println("My Name is "+Nama);
+        System.out.println("");
+    }
+}
---
+++ src/pbo2/pkg10118080/latihan31/mahasiswa/oop/PBO210118080Latihan31MahasiswaOop.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118080.latihan31.mahasiswa.oop;
+
+/**
+ *
+ * @author Trizky
+ */
+public class PBO210118080Latihan31MahasiswaOop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.Nim="10110269";
+        mhs.Nama="Rizki Adam Kurniawan";
+        mhs.perkenalkanDiri();
+        
+        mhs.Nim="10110270";
+        mhs.Nama="Indra Tiola";
+        mhs.perkenalkanDiri();
+        
+        mhs.Nim="10110271";
+        mhs.Nama="Robi Tanzil Ganefi";
+        mhs.perkenalkanDiri();
+        
+        mhs.Nim="10110272";
+        mhs.Nama="Muhammad Nur Awaludin";
+        mhs.perkenalkanDiri();
+    }
+    
+}
